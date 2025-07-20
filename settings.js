df5e9d29977c9e8fdc4c51e8e0642670ffe4792e
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2025-07-20 16:14:21 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YOUR_SESSION_ID",
+SESSION_ID: process.env.SESSION_ID || "QUEEN-ELISA~SUsX3YpI#HTPoKyWG_9ENKXvqIOaVeQKeIKyjy8GWgtNKdZWvIrc",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -13,39 +13,39 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY QUEEN ELISA INC*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY IRON-MAN-MD INC*",
 // set the auto reply massage on status reply  
 WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMG: process.env.MENU_IMG || "https://files.catbox.moe/0xh1qr.jpg",
+MENU_IMG: process.env.MENU_IMG || "https://files.catbox.moe/tmtn0f.jpeg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "~",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "QUEEN-ELISA",
+BOT_NAME: process.env.BOT_NAME || "Mudiwa-Mildred",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "QUEEN-ELISA",
+STICKER_NAME: process.env.STICKER_NAME || "Mudiwa-Mildred",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "true",
+DELETE_LINKS: process.env.DELETE_LINKS || "falsee",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "YOUR_NUMBER",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263777756184",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "A Y A N  D E V",
+OWNER_NAME: process.env.OWNER_NAME || "Joshumambo1-TECH",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*©made by ayan dev*",
+DESCRIPTION: process.env.DESCRIPTION || "*©made by https://youtube.com/@joshuamambo1 dev*",
 // add bot owner name    
-ALIVE_VIDEO: process.env.ALIVE_VIDEO || "https://files.catbox.moe/4qjiga.mp4",
+ALIVE_VIDEO: process.env.ALIVE_VIDEO || "https://files.catbox.moe/vpnq7.mp3",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *QUEEN-ELISA*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> Subscribe Hun Yar *Mudiwa-Mildred* https://youtube.com/@joshuamambo1⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -55,7 +55,7 @@ ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "true",
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // make anti link true,false for groups 
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
@@ -71,14 +71,14 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "YOUR_NUMBER",
+DEV: process.env.DEV || "263738403205",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
 // change it to 'same' if you want to resend deleted message in same chat 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 // make it true for auto recoding
 
-BOT_NUMBER: process.env.BOT_NUMBER || "YOUR_NUMBER"    
+BOT_NUMBER: process.env.BOT_NUMBER || "263777756184"    
 };
